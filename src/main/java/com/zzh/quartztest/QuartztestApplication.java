5415a97da56fd935cfe6cbd0f487f937a40b5648
gnuzman <94644378@qq.com>
2017-08-24 16:25:03 +0800

    change to spring boot

---
+++ src/main/java/com/zzh/quartztest/QuartztestApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.quartztest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartztestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartztestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zzh/quartztest/QuartztestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzh.quartztest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartztestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
