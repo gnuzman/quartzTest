64a5fabf24520cc85ff0e1273db57f2459601bc9
gnuzman <94644378@qq.com>
2017-08-28 16:47:01 +0800

    not success

---
+++ src/main/java/com/zzh/quartztest/util/CustomProperties.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.zzh.quartztest.util;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author pavan.solapure
+ *
+ */
+public class CustomProperties extends Properties {
+
+	private final AppLogger logger = AppLogger.getInstance();
+	private static final long serialVersionUID = 1L;
+
+	public CustomProperties(DataSource dataSource) {
+		super();
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		List<Map<String, Object>> configs = jdbcTemplate
+				.queryForList("select config_key, config_value from config_params");
+		
+		logger.info("Loading properties from Database");
+		for (Map<String, Object> config : configs) {
+			setProperty((config.get("config_key")).toString(), (config.get("config_value")).toString());
+		}
+	}
+}
